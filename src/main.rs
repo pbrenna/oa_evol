edc87b0282add963629bcb19f9c40f9339c9ad30
Pietro Brenna <pietrobrenna@hotmail.it>
2018-09-17 10:37:03 +0200

    Aggiunto spiril forkato; ctrlc per fermare, barra di progresso, doc.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
 // This example implements the queens problem:
 // https://en.wikipedia.org/wiki/Eight_queens_puzzle
 // using an evolutionary algorithm.
-
+#![feature(nll)]
+extern crate ctrlc;
+extern crate pbr;
 extern crate rand;
-extern crate simplelog;
 
 // internal crates
 
-extern crate itertools;
 extern crate spiril;
 
-use itertools::Itertools;
 use rand::Rng;
 use std::iter;
 
 use spiril::{population::Population, unit::Unit};
+mod t_combinations;
 
 #[allow(non_snake_case)]
 #[derive(Clone, Debug)]
+/// Array ortogonale di dimensione ngrande * k, che si vuole portare a forza t.
 struct OArray {
     d: Vec<bool>,
-    N: usize,
+    ngrande: usize,
     k: usize,
+    target_t: usize,
 }
 
-#[allow(non_upper_case_globals)]
-const k: usize = 4;
+//Parametri di esecuzione
+const K: usize = 8;
 const N: usize = 16;
-#[allow(non_upper_case_globals)]
-const t: usize = 2;
+const T: usize = 2;
 
+/// Unisce due OArray in in modo che il risultato sia bilanciato
 fn balanced_crossover(a: &[bool], b: &[bool], out: &mut [bool], r: &mut impl Rng) {
     let ngrande = a.len();
     let mut cnt0 = 0usize;
@@ -53,35 +55,45 @@ fn balanced_crossover(a: &[bool], b: &[bool], out: &mut [bool], r: &mut impl Rng
 }
 
 impl OArray {
-    fn new_random_balanced(my_k: usize, ngrande: usize, rng: &mut impl rand::Rng) -> Self {
+    /// Crea un array di larghezza `my_k` * `ngrande`,
+    /// che si vorrà portare ad avere forza `t`, e lo inizializza
+    /// in modo randomico ma bilanciato utilizzando `rng`.
+    fn new_random_balanced(
+        my_k: usize,
+        ngrande: usize,
+        target_t: usize,
+        rng: &mut impl rand::Rng,
+    ) -> Self {
         assert!(
-            ngrande / (2usize.pow(t as u32)) >= 1,
+            ngrande / (2usize.pow(target_t as u32)) >= 1,
             "I parametri N={},s=2,t={} non soddisfano i requisiti base per un array ortogonale",
             ngrande,
-            t
+            target_t
         );
         let mut out = OArray {
             d: iter::repeat(&[true, false])
                 .map(|i| *rng.choose(i).unwrap())
                 .take(ngrande * my_k)
                 .collect(),
-            N: ngrande,
+            ngrande: ngrande,
             k: my_k,
+            target_t: target_t,
         };
-        for x in 0..k {
-            rng.shuffle(&mut out.d[x * ngrande..x * ngrande + ngrande]);
+        for x in 0..K {
+            rng.shuffle(&mut out.d[(x * ngrande)..(x * ngrande + ngrande)]);
         }
         out
     }
 
+    /// conta il numero di occorrenze di `needle` nelle colonne `igrande` dell'array,
+    /// e restituisce la differenza rispetto al livello `lambda`
     fn delta(&self, igrande: &[usize], needle: usize, lambda: usize) -> usize {
         let mut out = 0;
-        for i in 0..self.N {
+        for i in 0..self.ngrande {
             //iterate rows
-            let cur_row = igrande
-                .iter()
-                .rev()
-                .fold(0, |acc, col| acc << 1 | (self.d[col * self.N + i] as usize));
+            let cur_row = igrande.iter().rev().fold(0, |acc, col| {
+                acc << 1 | (self.d[col * self.ngrande + i] as usize)
+            });
             if cur_row == needle {
                 out += 1
             }
@@ -89,10 +101,12 @@ impl OArray {
         (lambda as isize - out as isize).abs() as usize
     }
 
+    /// calcola per ogni numero rappresentabile da `igrande.len` bit
+    /// la funzione delta, usa i risultati per dare una distanza.
     fn delta_grande(&self, igrande: &[usize], p: f64) -> f64 {
         let t_num = igrande.len();
         let max_binary = 2usize.pow(t_num as u32) - 1;
-        let lambda = self.N / max_binary;
+        let lambda = self.ngrande / max_binary;
         let result = (0..max_binary)
             .map(|i| {
                 let d = self.delta(igrande, i, lambda);
@@ -101,12 +115,20 @@ impl OArray {
             .powf(1.0 / p);
         result
     }
+
+    /// Scambia due coordinate nel vettore con probabiltà `prob`,
+    /// usando `rng`.
     fn mutate_with_prob(&mut self, prob: f64, rng: &mut impl Rng) {
         let rand = rng.gen_range::<f64>(0.0, 1.0);
         if rand < prob {
             //pick random coordinate to mutate
-            let coord = rng.gen_range(0, self.N * self.k);
-            self.d[coord] = !self.d[coord]
+            let coord1 = rng.gen_range(0, self.ngrande * self.k);
+            //pick other coordinate to swap
+            let mut coord2 = rng.gen_range(0, self.ngrande * self.k);
+            while self.d[coord2] == self.d[coord1] {
+                coord2 = rng.gen_range(0, self.ngrande * self.k);
+            }
+            self.d.swap(coord1, coord2);
         }
     }
 }
@@ -117,35 +139,45 @@ impl Unit for OArray {
         //GA crossover and mutation operators are applied
         //component-wise on each bitstring
         let mut out = OArray {
-            d: iter::repeat(false).take(self.N * self.k).collect(),
-            N: self.N,
+            d: iter::repeat(false).take(self.ngrande * self.k).collect(),
+            ngrande: self.ngrande,
             k: self.k,
+            target_t: self.target_t,
         };
-        for i in 0..k {
-            let ngrande = self.N;
+        for i in 0..K {
+            let ngrande = self.ngrande;
             let col1 = &self.d[i * ngrande..i * ngrande + ngrande];
             let col2 = &other.d[i * ngrande..i * ngrande + ngrande];
             let col3 = &mut out.d[i * ngrande..i * ngrande + ngrande];
             balanced_crossover(col1, col2, col3, &mut rng);
         }
-        out.mutate_with_prob(0.02, &mut rng);
+        out.mutate_with_prob(0.25, &mut rng);
         out
     }
 
-    // fitness means here: how many queens are colliding
+    /// Fitness: calcola delta_grande per ogni combinazione di colonne e somma
     fn fitness(&self) -> f64 {
-        let asd: f64 = (0..self.k)
-            .combinations(t)
+        let asd: f64 = t_combinations::Combinations::new(self.k, self.target_t)
+            .iter()
             .map(|igrande| self.delta_grande(&igrande, 2.0))
             .sum();
         -asd
     }
 }
 impl std::fmt::Display for OArray {
+    /// Stampa un OA
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        for i in 0..self.N {
+        for i in 0..self.ngrande {
             for j in 0..self.k {
-                write!(f, "{}", if self.d[j * self.N + i] { "1 " } else { "0 " });
+                write!(
+                    f,
+                    "{}",
+                    if self.d[j * self.ngrande + i] {
+                        "1 "
+                    } else {
+                        "0 "
+                    }
+                );
             }
             writeln!(f, "");
         }
@@ -153,23 +185,35 @@ impl std::fmt::Display for OArray {
     }
 }
 
-fn epoch_callback(p : &Population<OArray>, i: usize, best: f64) {
-    println!("Epoch {}, best fitness {}", i, best);
-    
-}
-
 fn main() {
     let mut rng = rand::thread_rng();
-    let units: Vec<OArray> = (0..1000)
-        .map(|_i| OArray::new_random_balanced(k, N, &mut rng))
+    let units: Vec<OArray> = (0..2000)
+        .map(|_i| OArray::new_random_balanced(K, N, T, &mut rng))
         .collect();
 
+    let epochs = 3000;
+    let mut bar = pbr::ProgressBar::new(epochs);
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    ctrlc::set_handler(move || {
+        tx.send(()).unwrap();
+    }).expect("Can't register ctrl+c");
     let f = Population::new(units)
-        .set_size(1000)
-        .set_breed_factor(0.3)
-        .set_survival_factor(0.6)
-        .register_epoch_callback(&epoch_callback)
-        .epochs_parallel(500, 4) // 4 CPU cores
+        .set_size(2000)
+        .set_breed_factor(0.4)
+        .set_survival_factor(0.8)
+        .register_callback(Box::new(move |i, j, count| {
+            bar.message(&format!(
+                " Best: {:.4}, Mean: {:.4}, Count: {}; iteration ",
+                i, j, count
+            ));
+            (&mut bar).inc();
+            if let Ok(_) = rx.try_recv() {
+                false
+            } else {
+                true
+            }
+        })).epochs_parallel(epochs as u32, 4) // 4 CPU cores
         .finish();
     let asd = f
         .iter()
@@ -179,16 +223,28 @@ fn main() {
 
 #[test]
 fn new_random() {
-    let a = OArray::new_random_balanced(k, N, &mut rand::thread_rng());
+    let a = OArray::new_random_balanced(K, N, T, &mut rand::thread_rng());
     println!("{:?}", a);
 }
 
 #[test]
-fn check_fitness() {
+fn check_fitness1() {
     let test = OArray {
         d: vec![false, false, true, true, false, true, false, true],
-        N: 4,
+        ngrande: 4,
         k: 2,
+        target_t: 2,
     };
-    println!("Fitnesssss: {}", test.fitness());
+    assert!(test.fitness() == 0.0);
+}
+
+#[test]
+fn check_fitness2() {
+    let test = OArray {
+        d: vec![false, true, true, true, false, true, false, true],
+        ngrande: 4,
+        k: 2,
+        target_t: 1,
+    };
+    assert!(test.fitness() != 0.0);
 }
---
+++ src/t_combinations.rs
@@ -0,0 +1,107 @@
+//! Genera tutte le combinazioni di t elementi tra n,
+//! utilizzando l'algoritmo L di Knuth
+
+/// Struttura di appoggio per generare le combinazioni.
+pub struct Combinations {
+    /// numero elementi da cui generare le combinazioni
+    n : usize,
+    /// lunghezza delle combinazioni generate
+    t: usize,
+    /// vettore di appoggio
+    c: Vec<usize>,
+}
+
+/// oggetto iteratore. Stato della generazione delle combinazioni
+pub struct CombinationsIter<'a> {
+    comb: &'a mut Combinations,
+    initial: bool,
+}
+
+impl Combinations
+where
+{
+    pub fn new(n: usize, t: usize) -> Self {
+        Combinations { n: n, t: t, c: Vec::new() }
+    }
+    /// Restituisce un iteratore sulle combinazioni
+    pub fn iter(&mut self) -> CombinationsIter {
+        let mut v: Vec<usize> = (0 .. self.t).collect();
+        v.push(self.n);
+        v.push(0);
+        self.c = v;
+        CombinationsIter {
+            comb: self,
+            initial: true,
+        }
+    }
+}
+
+impl<'a> Iterator for CombinationsIter<'a>
+{
+    type Item = Vec<usize>;
+    /// Algoritmo L di Knuth
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.initial {
+            if self.comb.t == 0
+                { return None; }
+            self.initial = false;
+        } else {
+
+            let mut j = 0usize;
+            {
+                let c = &mut self.comb.c;
+                while c[j] + 1 == c[j+1] {
+                    let tmp = j;
+                    c[j] = tmp.into();
+                    j = j + 1;
+                }
+                c[j] = c[j] + 1;
+            }
+            if j >= self.comb.t {
+                return None;
+            } 
+        }
+        Some(Vec::from(&self.comb.c[0..self.comb.t.into()]))
+    }
+}
+
+#[allow(dead_code)]
+fn check_result(results: Vec<&[usize]>, c: impl Iterator<Item = Vec<usize>>){
+    let mut tot = 0;
+    for (i, result) in c.enumerate() {
+        assert!(result == results[i]);
+        tot += 1;
+    }
+    assert!(tot == results.len());
+}
+
+#[test]
+fn test0(){
+    let mut c = Combinations::new(2, 0);
+    let results: Vec<&[usize]> = vec![];
+    check_result(results, c.iter());
+}
+
+
+#[test]
+fn test1(){
+    let mut c = Combinations::new(2, 1);
+    let results: Vec<&[usize]> = vec![
+        &[0],
+        &[1]
+    ];
+    check_result(results, c.iter());
+}
+
+
+#[test]
+fn test2() {
+    let mut c = Combinations::new(4, 3);
+    let results : Vec<&[usize]> = vec![
+        &[0usize, 1, 2],
+        &[0, 1, 3],
+        &[0, 2, 3],
+        &[1, 2, 3]
+    ];
+    check_result(results, c.iter());
+}
