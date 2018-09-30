f402eb00895ba54bf93536f27090a306ddb1dc76
Pietro Brenna <pietrobrenna@hotmail.it>
2018-09-30 14:18:05 +0200

    Generic value s for an OA

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate ctrlc;
+extern crate num;
+extern crate num_iter;
+extern crate num_traits;
 extern crate pbr;
 extern crate rand;
 
@@ -9,17 +12,39 @@ extern crate spiril;
 use rand::Rng;
 use std::iter;
 
+use num::Integer;
+use num_iter::range;
+use num_traits::{FromPrimitive, One, ToPrimitive, Zero};
 use spiril::{population::Population, unit::Unit};
 mod epoch;
 mod t_combinations;
 use epoch::TournamentEpoch;
+use std::ops::Add;
+
+trait Alphabet:
+    Integer + Clone + Add<Self, Output = Self> + One + Zero + FromPrimitive + ToPrimitive + Send + Copy
+{
+}
+impl<
+        T: Integer
+            + Clone
+            + Add<Self, Output = Self>
+            + One
+            + Zero
+            + FromPrimitive
+            + ToPrimitive
+            + Send
+            + Copy,
+    > Alphabet for T
+{}
 
 #[derive(Clone, Debug)]
 /// Array ortogonale di dimensione ngrande * k, che si vuole portare a forza t.
-struct OArray {
-    d: Vec<bool>,
+struct OArray<T: Alphabet> {
+    d: Vec<T>,
     ngrande: usize,
     k: usize,
+    s: T,
     target_t: usize,
 }
 
@@ -27,31 +52,49 @@ struct OArray {
 const K: usize = 8;
 const N: usize = 16;
 const T: usize = 3;
+const S: u8 = 2;
 
 /// Unisce due OArray in in modo che il risultato sia bilanciato
-fn balanced_crossover(a: &[bool], b: &[bool], out: &mut [bool], r: &mut impl Rng) {
+fn balanced_crossover<T: Alphabet>(a: &[T], b: &[T], out: &mut [T], s: T, r: &mut impl Rng) {
     let ngrande = a.len();
-    let mut cnt0 = 0usize;
-    let mut cnt1 = 0usize;
+    let s_size = s.to_usize().unwrap();
+    let balance = ngrande / s_size;
+    assert!(balance * s_size == ngrande);
+    let mut cnt = vec![balance; s_size];
     let mut pos: Vec<_> = (0..ngrande).collect();
     r.shuffle(&mut pos);
-    for i in pos {
-        if cnt0 == ngrande / 2 {
-            out[i] = true;
-        } else if cnt1 == ngrande / 2 {
-            out[i] = false;
+    for j in pos {
+        let a_usize = a[j].to_usize().unwrap();
+        let b_usize = b[j].to_usize().unwrap();
+        let choice;
+        //If choosing neither a or b affects balancedness..
+        if cnt[a_usize] > 0 && cnt[b_usize] > 0 {
+            //choose randomly
+            choice = *r.choose(&[a[j], b[j]]).unwrap();
+        } else if cnt[a_usize] == 0 && cnt[b_usize] > 0 {
+            //only b can be choosen
+            choice = b[j];
+        } else if cnt[a_usize] > 0 && cnt[b_usize] == 0 {
+            //only a can be choosen
+            choice = a[j];
         } else {
-            out[i] = *r.choose(&[a[i], b[i]]).unwrap();
-            if out[i] {
-                cnt1 += 1;
-            } else {
-                cnt0 += 1;
-            }
+            //choose randomly amongst all symbols that don't affect balancedness
+            let possible_choices: Vec<T> = range(T::zero(), s)
+                .enumerate()
+                .filter(|&(i, _)| cnt[i] > 0)
+                .map(|(_, j)| j)
+                .collect();
+            choice = *r.choose(&possible_choices).unwrap();
         }
+        assert!(choice < s);
+
+        out[j] = choice;
+        let choice_u = choice.to_usize().unwrap();
+        cnt[choice_u] -= 1;
     }
 }
 
-impl OArray {
+impl<T: Alphabet> OArray<T> {
     /// Crea un array di larghezza `k` * `ngrande`,
     /// che si vorrà portare ad avere forza `t`, e lo inizializza
     /// in modo randomico ma bilanciato utilizzando `rng`.
@@ -59,24 +102,23 @@ impl OArray {
         k: usize,
         ngrande: usize,
         target_t: usize,
+        s: T,
         rng: &mut impl rand::Rng,
     ) -> Self {
+        let s_usize = s.to_usize().unwrap();
         assert!(
-            ngrande / (2usize.pow(target_t as u32)) >= 1,
-            "I parametri N={},s=2,t={} non soddisfano i requisiti base per un array ortogonale",
+            ngrande / (s_usize.pow(target_t as u32)) >= 1,
+            "I parametri N={},s={},t={} non soddisfano i requisiti base per un array ortogonale",
             ngrande,
+            s_usize,
             target_t
         );
-        let mut out = OArray {
-            //ripete true,false ngrande*k volte
-            d: [true, false]
-                .iter()
-                .cloned()
-                .cycle()
-                .take(ngrande * k)
-                .collect(),
+        let mut out: OArray<T> = OArray {
+            //ripete l'alfabeto ngrande*k volte
+            d: range(T::zero(), s).cycle().take(ngrande * k).collect(),
             ngrande,
             k,
+            s,
             target_t,
         };
         //mescola ogni colonna
@@ -90,10 +132,11 @@ impl OArray {
     /// e restituisce la differenza rispetto al livello `lambda`
     fn delta(&self, igrande: &[usize], needle: usize, lambda: usize) -> usize {
         let mut out = 0;
+        let base = self.s.to_usize().unwrap();
         for i in 0..self.ngrande {
             //iterate rows
-            let cur_row = igrande.iter().rev().fold(0, |acc, col| {
-                acc << 1 | (self.d[col * self.ngrande + i] as usize)
+            let cur_row = igrande.iter().fold(0, |acc, col| {
+                acc * base + self.d[col * self.ngrande + i].to_usize().unwrap()
             });
             if cur_row == needle {
                 out += 1
@@ -134,35 +177,38 @@ impl OArray {
         }
     }
 
-    fn iter_cols(&self) -> impl Iterator<Item = &[bool]> {
+    fn iter_cols(&self) -> impl Iterator<Item = &[T]> {
         self.d.chunks(self.ngrande)
     }
-    fn iter_cols_mut(&mut self) -> impl Iterator<Item = &mut [bool]> {
+    fn iter_cols_mut(&mut self) -> impl Iterator<Item = &mut [T]> {
         self.d.chunks_mut(self.ngrande)
     }
-    fn iter_rows(&self) -> impl Iterator<Item = Vec<&bool>> {
+    fn iter_rows(&self) -> impl Iterator<Item = Vec<&T>> {
         let b = self.ngrande;
         (0..b).map(move |i| (&self.d[i..]).iter().step_by(b).collect())
     }
 }
 // implement trait functions mutate and calculate_fitness:
-impl Unit for OArray {
+impl<T: Alphabet> Unit for OArray<T> {
     fn breed_with(&self, other: &Self) -> Self {
         //println!("BREED++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         let mut rng = rand::thread_rng();
         //GA crossover and mutation operators are applied
         //component-wise on each bitstring
-        let mut out = OArray {
-            d: iter::repeat(false).take(self.ngrande * self.k).collect(),
+        let mut out: OArray<T> = OArray {
+            d: iter::repeat(T::zero())
+                .take(self.ngrande * self.k)
+                .collect(),
             ngrande: self.ngrande,
             k: self.k,
+            s: self.s,
             target_t: self.target_t,
         };
         for (col1, (col2, col3)) in self
             .iter_cols()
             .zip(other.iter_cols().zip(out.iter_cols_mut()))
         {
-            balanced_crossover(col1, col2, col3, &mut rng);
+            balanced_crossover(col1, col2, col3, self.s, &mut rng);
         }
         out.mutate_with_prob(0.2, &mut rng);
         out
@@ -177,7 +223,7 @@ impl Unit for OArray {
         -asd
     }
 }
-impl std::fmt::Display for OArray {
+impl<T: Alphabet> std::fmt::Display for OArray<T> {
     /// Stampa un OA
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         let fit = self.fitness();
@@ -190,7 +236,8 @@ impl std::fmt::Display for OArray {
         }
         for row in self.iter_rows() {
             for x in row {
-                write!(f, "{} ", *x as u8)?
+                let x_conv = x.to_usize().unwrap();
+                write!(f, "{} ", x_conv)?
             }
             writeln!(f)?
         }
@@ -202,8 +249,9 @@ fn main() {
     let n_units = 50;
     let epochs = 10000;
     let mut rng = rand::thread_rng();
-    let units: Vec<OArray> = (0..n_units)
-        .map(|_i| OArray::new_random_balanced(K, N, T, &mut rng))
+
+    let units: Vec<OArray<_>> = (0..n_units)
+        .map(|_i| OArray::new_random_balanced(K, N, T, S, &mut rng))
         .collect();
 
     let mut pbar = pbr::ProgressBar::new(epochs);
@@ -214,7 +262,7 @@ fn main() {
     }).expect("Can't register ctrl+c");
 
     let epoch = TournamentEpoch::new(500_000);
-    //let epoch = spiril::epoch::DefaultEpoch::new(0.2,0.8);
+    let epoch = spiril::epoch::DefaultEpoch::new(0.2, 0.8);
     let f = Population::new(units)
         .set_size(n_units)
         .set_breed_factor(0.2)
@@ -226,7 +274,7 @@ fn main() {
             }*/
             (&mut pbar).inc();
             rx.try_recv().is_err()
-        })).epochs_parallel(epochs as u32, 4, epoch) // 4 CPU cores
+        })).epochs_parallel(epochs as u32, 4, &epoch) // 4 CPU cores
         .finish();
     let asd = f
         .iter()
@@ -241,10 +289,10 @@ fn main() {
 
 #[test]
 fn new_random() {
-    let a = OArray::new_random_balanced(K, N, T, &mut rand::thread_rng());
+    let a = OArray::new_random_balanced(K, N, T, 2u8, &mut rand::thread_rng());
     for col in a.iter_cols() {
-        let num0 = col.iter().filter(|&&i| !i).count();
-        let num1 = col.iter().filter(|&&i| i).count();
+        let num0 = col.iter().filter(|&&i| i == 1).count();
+        let num1 = col.iter().filter(|&&i| i == 0).count();
         assert!(num0 == num1);
     }
 }
@@ -252,7 +300,7 @@ fn new_random() {
 #[test]
 fn mutation() {
     let mut r = rand::thread_rng();
-    let mut a = OArray::new_random_balanced(K, N, T, &mut r);
+    let mut a = OArray::new_random_balanced(K, N, T, 2u8, &mut r);
     let b = a.clone();
     assert!(a.d == b.d);
     a.mutate_with_prob(1.0, &mut r);
@@ -265,9 +313,10 @@ fn mutation() {
 #[test]
 fn check_fitness1() {
     let test = OArray {
-        d: vec![false, false, true, true, false, true, false, true],
+        d: vec![0, 0, 1, 1, 0, 1, 0, 1],
         ngrande: 4,
         k: 2,
+        s: 2u8,
         target_t: 2,
     };
     assert!(test.fitness() == 0.0);
@@ -276,9 +325,10 @@ fn check_fitness1() {
 #[test]
 fn check_fitness2() {
     let test = OArray {
-        d: vec![false, true, true, true, false, true, false, true],
+        d: vec![0, 1, 1, 1, 0, 1, 0, 1],
         ngrande: 4,
         k: 2,
+        s: 2u8,
         target_t: 1,
     };
     assert!(test.fitness() != 0.0);
