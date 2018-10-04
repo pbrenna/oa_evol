5954e3bd39ac7d64fdb3c8e4a7988c14d43310d4
Pietro Brenna <pietrobrenna@hotmail.it>
2018-10-04 14:59:55 +0200

    Use hard pruning

--- gp/src/main.rs
+++ gp/src/main.rs
@@ -20,28 +20,39 @@ mod treeformula;
 use gpoarray::GPOArray;
 
 fn main() {
-    let n = 5; //N = 2^n
-    let k = 16;
+    let n = 4; //N = 2^n
+    let k = 15;
     let t = 2;
     let pop_size = 500;
     println!("Looking for OA({}, {}, 2, {})", 2usize.pow(n as u32), k, t);
 
-    let leaf_bias = 0.8;
+    let max_depth = n;
+    //let leaf_bias = 0.8;
     let mutation_prob = 0.5;
-    let crossover = Crossover::one_point_leaf_biased(leaf_bias);
+    let crossover = Crossover::hard_prune(max_depth);
+    //let crossover = Crossover::one_point_leaf_biased(leaf_bias);
     let mutation = Mutation::uniform();
     let epochs = 1000;
     let epoch = spiril::epoch::DefaultEpoch::new(0.2, 0.8);
 
-    let max_depth = n;
     //let mut rng = OsRng::new().unwrap();
     //let tree_gen = TreeGen::full(&mut rng, 1, 4);
 
     let rng = OsRng::new().unwrap();
     let population: Vec<GPOArray<_>> = (0..pop_size)
-        .map(|_| GPOArray::new_rand(n, k, t, max_depth, rng.clone(), crossover, mutation, mutation_prob))
+        .map(|_| {
+            GPOArray::new_rand(
+                n,
+                k,
+                t,
+                max_depth,
+                rng.clone(),
+                crossover,
+                mutation,
+                mutation_prob,
+            )
+        })
         .collect();
-
 
     let mut pb = ProgressBar::new(epochs);
 
@@ -50,7 +61,6 @@ fn main() {
         tx.send(()).unwrap();
     })
     .unwrap();
-
 
     let f = Population::new(population)
         .set_size(pop_size)
@@ -66,7 +76,8 @@ fn main() {
         .finish();
     let asd = f
         .iter()
-        .max_by(|&a, &b| a.fitness().partial_cmp(&b.fitness()).unwrap()).unwrap();
+        .max_by(|&a, &b| a.fitness().partial_cmp(&b.fitness()).unwrap())
+        .unwrap();
     if -asd.fitness() < f64::EPSILON {
         println!("\n\n{}\n\n{}\n\n", asd.to_oarray(), asd);
         std::process::exit(2);
