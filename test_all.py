d2f9c89348531cd42bc5816ec8ae7e4c4d076dfc
Pietro Brenna <pietrobrenna@hotmail.it>
2019-01-01 20:47:25 +0100

    auto generate makefile with test_all.py

---
+++ test_all.py
@@ -0,0 +1,39 @@
+import csv
+
+makefile = "threads = 2"
+all = ""
+runs = 100
+with open("tests.csv") as f:
+    csv_reader = csv.reader(f, delimiter='\t')
+    line = 0
+    for row in csv_reader:
+        line += 1
+        if row[0] == "":
+            continue
+        algo = row[0]
+        part1 =""
+        folder = "ga_algo"
+        fitness = row[1]
+        exponent = row[2]
+        depth = row[3]
+        parts = row[4].split(",")
+        N = parts[0]
+        k = parts[1]
+        t = parts[2]
+        outfile = "{:03d}.{}.{}.{}.{}".format(line, algo, N, k, t)
+        if exponent != "":
+            outfile += ".exp{}".format(exponent)
+        if depth != "":
+            outfile += ".depth{}".format(depth)
+        outfile += ".{}runs.log".format(runs)
+        if exponent == "":
+            exponent = 2
+        if algo == "gp":
+            folder = "gp_algo"
+            part1 = "--depth {}".format(depth) if depth!="" else ""
+        makefile += """\nresults/{}:
+\tcd {} && cargo run --release {} {} {} --fitness {} --fitness-exp {} {} --runs {} --log ../results/{}.tmp --threads $(threads)
+""".format(outfile, folder, N, k, t, fitness, exponent, part1, runs, outfile)
+        all+=" results/{}".format(outfile)
+makefile += "\nall:{}".format(all)
+print(makefile)
