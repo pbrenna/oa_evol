15098c54fead5902c87ec4c1592401135563588a
Pietro Brenna <pietrobrenna@hotmail.it>
2019-02-02 12:05:25 +0100

    remove balanced hack

--- test_all.py
+++ test_all.py
@@ -5,8 +5,6 @@ makefile = ""
 fitness_map = {
     'delta': 'DeltaFast',
     'walsh': 'WalshFast',
-    'delta_bal' : 'DeltaFast',
-    'walsh_bal' : 'WalshFast'
 }
 with open("tests.csv") as f:
     csv_reader = csv.reader(f, delimiter='\t')
